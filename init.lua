68cd367ac21ba9c868185b5ee71cd915d6fabd25
Brian <rabbinero@gmail.com>
2024-11-23 14:33:08 -0500

    small config changes: leader to '\' and commenting out which-key nerd-font alternatives

--- init.lua
+++ init.lua
@@ -87,8 +87,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = '\\'
+vim.g.maplocalleader = '\\'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
@@ -281,34 +281,34 @@ require('lazy').setup({
         -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
         -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
         keys = vim.g.have_nerd_font and {} or {
-          Up = '<Up> ',
-          Down = '<Down> ',
-          Left = '<Left> ',
-          Right = '<Right> ',
-          C = '<C-…> ',
-          M = '<M-…> ',
-          D = '<D-…> ',
-          S = '<S-…> ',
-          CR = '<CR> ',
-          Esc = '<Esc> ',
-          ScrollWheelDown = '<ScrollWheelDown> ',
-          ScrollWheelUp = '<ScrollWheelUp> ',
-          NL = '<NL> ',
-          BS = '<BS> ',
-          Space = '<Space> ',
-          Tab = '<Tab> ',
-          F1 = '<F1>',
-          F2 = '<F2>',
-          F3 = '<F3>',
-          F4 = '<F4>',
-          F5 = '<F5>',
-          F6 = '<F6>',
-          F7 = '<F7>',
-          F8 = '<F8>',
-          F9 = '<F9>',
-          F10 = '<F10>',
-          F11 = '<F11>',
-          F12 = '<F12>',
+          -- Up = '<Up> ',
+          -- Down = '<Down> ',
+          -- Left = '<Left> ',
+          -- Right = '<Right> ',
+          -- C = '<C-…> ',
+          -- M = '<M-…> ',
+          -- D = '<D-…> ',
+          -- S = '<S-…> ',
+          -- CR = '<CR> ',
+          -- Esc = '<Esc> ',
+          -- ScrollWheelDown = '<ScrollWheelDown> ',
+          -- ScrollWheelUp = '<ScrollWheelUp> ',
+          -- NL = '<NL> ',
+          -- BS = '<BS> ',
+          -- Space = '<Space> ',
+          -- Tab = '<Tab> ',
+          -- F1 = '<F1>',
+          -- F2 = '<F2>',
+          -- F3 = '<F3>',
+          -- F4 = '<F4>',
+          -- F5 = '<F5>',
+          -- F6 = '<F6>',
+          -- F7 = '<F7>',
+          -- F8 = '<F8>',
+          -- F9 = '<F9>',
+          -- F10 = '<F10>',
+          -- F11 = '<F11>',
+          -- F12 = '<F12>',
         },
       },
 
